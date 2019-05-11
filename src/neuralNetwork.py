f9a625dbd3fb12ebe46c076764b68236989842b6
robertgrochowski <robert.grochowski98@gmail.com>
2019-05-11 18:53:46 +0200

    .

--- src/neuralNetwork.py
+++ src/neuralNetwork.py
@@ -60,10 +60,9 @@ class NeuralNetwork:
         self.hiddenLayerBias = m.rand(hidden_layer_neurons, 1)
         self.outputLayerBias = m.rand(4, 1)
 
-
     def teach(self):
 
-        for epoch in range(50):
+        for epoch in range(400):
             error = 0
             for i in range(0, self.trainingSet.shape[0]):
                 # Xn:
@@ -85,6 +84,7 @@ class NeuralNetwork:
                     net = NET2.item(k, 0)
                     d = Dn.item(0, k)
                     z = Z.item(k, 0)
+
                     E.insert(k, self.sigmoid_bipolar_derivative(z) * (d - z))
 
                 # compute error for each hidden neuron
@@ -109,7 +109,6 @@ class NeuralNetwork:
 
                 # Kumuluj blad
                 for k in range(self.outputLayerNeurons):
-                    print(str(Dn.item(0, k)) + " " + str(Z.item(k, 0)))
                     error += pow((Dn.item(0, k) - Z.item(k, 0)), 2)
 
             error /= 2
@@ -122,12 +121,18 @@ class NeuralNetwork:
         output = np.mat(NETValue, dtype=float)
 
         for i in range(output.shape[0]):
-            output[i][0] = (2 / 1 + pow(math.e, -output.item(i, 0)*_lambda)) - 1
+            output[i][0] = self.sigmoid(output.item(i, 0))
 
         return output
 
+    def sigmoid(self, val):
+        return (2 / (1 + pow(math.e, val*-1*1))) - 1
 
 if __name__ == '__main__':
-    n = NeuralNetwork(7, 0.1, 1)
+    n = NeuralNetwork(17, 0.3, 1)
     n.teach()
+
+    #v = n.sigmoid(-1)
+    #print(v)
+    pass
 
